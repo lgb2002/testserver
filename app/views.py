5a36d7d3eae3e5c0eef082dbe83fbcec79bdea0c
lgb2002 <cccavin12345@naver.com>
2018-05-22 22:46:18 +0900

    hello

--- app/views.py
+++ app/views.py
@@ -1,42 +1,26 @@
-#from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-
+ 
 def keyboard(request):
-	return JsonResponse(
-		{
-		'type' : 'buttons',
-		'buttons' : ['hello', 'world', 'lgb'] 
-		}
-	)
-
+ 
+        return JsonResponse({
+                'type' : 'buttons',
+                'buttons' : ['1','2']
+                })
+ 
 @csrf_exempt
 def message(request):
-	json_str = (request.body).decode('utf-8')
-	received_json = json.loads(json_str) #JSON File Decoding
-	content_name = received_json['content']	
-	if content_name == 'hello':
-		return JsonResponse(
-			{
-				'message' : {
-					'text' : 'You say hello'
-				}
-			}
-		)
-	elif content_name == 'world':
-		return JsonResponse(
-			{
-				'message' : {
-					'text' : 'We are the world'
-				}
-			}
-		)
-	elif content_name == 'lgb':
-		return JsonResponse(
-			{
-				'message' : {
-					'text' : 'Your id is lgb'
-				}
-			}
-		)
+        message = ((request.body).decode('utf-8'))
+        return_json_str = json.loads(message)
+        return_str = return_json_str['content']
+ 
+        return JsonResponse({
+                'message': {
+                        'text': "you type "+return_str+"!"
+                },
+                'keyboard': {
+                        'type': 'buttons',
+                        'buttons': ['1','2']
+                }
+        })
