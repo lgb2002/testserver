b0505298c3911b8b9cac62c573d3138f23239f9b
lgb2002 <cccavin12345@naver.com>
2018-05-22 23:32:36 +0900

    hello

--- my_chatbot/urls.py
+++ my_chatbot/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from . import views
+from app import views
 
 urlpatterns = [
     url(r'^keyboard/',views.keyboard),
