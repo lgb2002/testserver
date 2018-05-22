401264eec863abda25c69e3dd84bee6d3ce97982
lgb2002 <cccavin12345@naver.com>
2018-05-22 17:48:46 +0900

    Changed the HTML for the site.

--- my_chatbot/settings.py
+++ my_chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&(-tsj+=7%4^a^_25j0(%+5da8l$wtd$un52*u7vp79%a$8l3a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'kakaotalkbot.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
