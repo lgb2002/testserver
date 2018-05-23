0007ab4730a18499fc5edfd9b1063f7a4fee161f
lgb2002 <cccavin12345@naver.com>
2018-05-24 00:43:04 +0900

    hello

--- app/views.py
+++ app/views.py
@@ -5,36 +5,42 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
+def get_html(url) :
+	_html = ""
+	resp = requests.get(url)
+	if resp.status_code == 200:
+		_html = resp.text
+	return _html
+
 datetime.today()
 year=datetime.today().year
 month=datetime.today().month
 day=datetime.today().day
 
-html = get_html("www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+year+"&domain.month="+month+"&domain.day="+day)
+imsi = "http://www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+str(year)+"&domain.month="+str(month)+"&domain.day="+str(day)
+html = get_html(imsi)
 soup = BeautifulSoup(html, 'html.parser')
 test = soup.find('<div class="Schoolmeal_Cont_Cont_Cont">')
 
 
 def keyboard(request):
- 
-        return JsonResponse({
-                'type' : 'buttons',
-                'buttons' : ['today','tommorow']
-                })
+	return JsonResponse({
+            'type' : 'buttons',
+            'buttons' : ['today','tommorow']
+            })
 
 @csrf_exempt
 def answer(request):
-        message = ((request.body).decode('utf-8')) 
-        return_json_str = json.loads(message)
-        return_str = return_json_str['content']
+	    message = ((request.body).decode('utf-8')) 
+	    return_json_str = json.loads(message)
+	    return_str = return_json_str['content']
 
-        if
-        return JsonResponse({
-                'message': {
-                        'text': "you choose!\n"+return_str" : \n"+test
-                },
-                'keyboard': {
-                        'type': 'buttons',
-                        'buttons': ['1','2']
-                }
-        })
+	    return JsonResponse({
+	        'message': {
+	            'text': "you choose!\n"+return_str+" : \n"+test
+	            },
+	            'keyboard': {
+	            'type': 'buttons',
+	           	'buttons' : ['today','tommorow']
+	            }
+	        })
