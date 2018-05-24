9bf37bab21b60534054605659260410abe6e7beb
lgb2002 <cccavin12345@naver.com>
2018-05-24 22:51:37 +0900

    hello

--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-#from bs4 import BeautifulSoup
-#import requests
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from datetime import datetime
 
@@ -13,9 +12,9 @@ day=datetime.today().day
 
 imsi = "http://www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+str(year)+"&domain.month="+str(month)+"&domain.day="+str(day)
 html = urlopen(imsi)
-#soup = BeautifulSoup(html.read(), "html.parser")
-#test = soup.find(class_="Schoolmeal_Cont_Cont_Cont")
-#print (test)
+soup = BeautifulSoup(html.read(), "html.parser")
+test = soup.find(class_="Schoolmeal_Cont_Cont_Cont")
+
 
 def keyboard(request):
 	return JsonResponse({
@@ -28,10 +27,11 @@ def answer(request):
 	    message = ((request1.body).decode('utf-8')) 
 	    return_json_str = json.loads(message)
 	    return_str = return_json_str['content']
+	    print (test)
 
 	    return JsonResponse({
 	        'message' : {
-	            'text' : "you choose!"+return_str+" : "#+test
+	            'text' : "you choose!"+return_str+" : "+test
 	            },
 	        'keyboard' : {
 	            'type': 'buttons',
