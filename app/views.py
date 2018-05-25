2a14928a228f064b50b6e02d3b8c56c865cf1799
lgb2002 <cccavin12345@naver.com>
2018-05-25 21:33:56 +0900

    Changed the HTML for the site.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+#from urllib.request import urlopen
 from datetime import datetime
 import re
 
@@ -12,13 +12,13 @@ month=datetime.today().month
 day=datetime.today().day
 
 imsi = "http://www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+str(year)+"&domain.month="+str(month)+"&domain.day="+str(day)
-html = urlopen(imsi)
-soup = BeautifulSoup(html.read(), "html.parser")
-test = soup.find(class_="Schoolmeal_Cont_Cont_Cont")
-test2 = test.get_text()
-m = re.sub(" ?\d ?[.]*"," ",test2)
-m = re.sub(" +","\n",m)
-print(m)
+#html = urlopen(imsi)
+#soup = BeautifulSoup(html.read(), "html.parser")
+#test = soup.find(class_="Schoolmeal_Cont_Cont_Cont")
+#test2 = test.get_text()
+#m = re.sub(" ?\d ?[.]*"," ",test2)
+#m = re.sub(" +","\n",m)
+#print(m)
  
 
 def keyboard(request):
@@ -35,7 +35,7 @@ def answer(request):
 
 	    return JsonResponse({
 	        'message' : {
-	            'text' : "you choose!"+return_str+" : "+test
+	            'text' : "you choose!"+return_str+" : "#+test
 	            },
 	        'keyboard' : {
 	            'type': 'buttons',
