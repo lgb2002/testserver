3240b681d7bd0dbd2d961ad18f785b16fef89ff9
lgb2002 <cccavin12345@naver.com>
2018-08-05 23:25:20 +0900

    help

--- runcode/views.py
+++ runcode/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import requests,json, os
+import requests, json, os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from bs4 import BeautifulSoup
@@ -10,9 +10,23 @@ def index(request):
 	return render(request, 'runcode/index.html', {})
 
 
+@csrf_exempt
 def run(request):
+
+	if request.method == "POST":
+		message = str(request.body, encoding='utf-8')
+		print("test message : " + message)
+		'''return_json_str = json.loads(message)
+		test = return_json_str['content']'''
+		'''Language = message[10 : 12]
+		Program = message[22 : len(message)-1]
+		print("Language : "+Language)
+		print("Program : "+Program)'''
+	test = open("data.txt", 'w')
+	test.write(message)
+
 	url = 'http://rextester.com/rundotnet/Run'
-	data ={'LanguageChoiceWrapper' : '38' ,
+	'''data = {'LanguageChoiceWrapper' : '38' ,
 			'EditorChoiceWrapper' : '1' ,
 			'LayoutChoiceWrapper' : '1' ,
 			'Input' : '' ,
@@ -25,18 +39,22 @@ def run(request):
 			'StatsToSave' : '' ,
 			'CodeGuid' : '' ,
 			'IsInEditMode' : 'False' ,
-			'IsLive' : 'False' ,
-			'Program' : '#!/bin/bash # GNU bash, version 4.3.46 echo "Hello, world!";' }
-	res = requests.post(url, data=data)
+			'IsLive' : 'False' 
+			}'''
+
+	'''data['Program'] = message'''
+
+	'''print("data['Program']: "+data['Program'])'''
+	files = {'file' : open('data.txt', 'rb')}
+	res = requests.post(url, files=files)
 	j = res.json()
-	'''print(data)'''
 	jsonString = json.dumps(j, indent=4)
 	print(jsonString)
 	dict = json.loads(jsonString)
 	warnings = str(dict['Warnings'])
 	errors = str(dict['Errors']) 
-	result = dict['Result']
-	stats = dict['Stats']
+	result = str(dict['Result'])
+	stats = str(dict['Stats'])
 	'''
 	print(res.text)
 	print("result :"+result)
