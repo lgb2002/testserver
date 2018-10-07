692aa779925b2908d908b662598842890b736e9f
lgb2002 <cccavin12345@naver.com>
2018-10-07 22:28:45 +0900

    test

--- app/views.py
+++ app/views.py
@@ -122,11 +122,10 @@ def answer(request) :
 			})
 		elif choice == 2:
 			message = return_str
+			print("message : "+message)
 			url = "http://kakao.pythonanywhere/runcode/run"
-			headers = {'Host': 'kakao.pythonanywhere.com', 'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Origin': 'http://kakao.pythonanywhere.com',
-'Upgrade-Insecure-Requests': '1', 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Referer': 'http://kakao.pythonanywhere.com/runcode/run', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-'Cookie': '_ga=GA1.2.1355811774.1532426694; csrftoken=gifaHd6LjiCpxsRAvP4EQwJLnRQNmxhLg6ehmTBJfYxs7HYhbBE57XVpS593aNnh; _gid=GA1.2.1446735685.1538794759'}
+			headers = {'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Referer': 'http://kakao.pythonanywhere.com/runcode/run', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'}
 			res = requests.post(url, headers=headers , data=message)
 			j = json.loads(res.text)
 			jsonString = json.dumps(j, indent=4)
