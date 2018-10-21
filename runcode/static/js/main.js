2c49e7d65e269e2f9821622517601cbe68959e09
lgb2002 <cccavin12345@naver.com>
2018-10-21 17:47:21 +0900

    hi

--- runcode/static/js/main.js
+++ runcode/static/js/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '/img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '/img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '/img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '/img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
+											{image : '{% static "/img/slider-images/image01.jpg" %}', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+											{image : '{% static "/img/slider-images/image02.jpg" %}', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+											{image : '{% static "/img/slider-images/image03.jpg" %}', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+											{image : '{% static "/img/slider-images/image04.jpg" %}', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
