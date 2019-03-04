6eba421dc65e4e983c2a180404b8e538e4b6208d
lgb2002 <cavin12345@naver.com>
2019-03-04 20:45:56 +0900

    90

--- app/views.py
+++ app/views.py
@@ -70,7 +70,7 @@ def get_menu(day) :
 		print("test : "+test)'''
 		
 		#test = "I will statrt the service tomorrow. Sorry;"
-		return test
+		return "hi"
 	except AttributeError:
 		test = "error"
 		return 
