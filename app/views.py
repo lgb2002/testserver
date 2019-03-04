8288d313f99cd4261d87083a49bacf8bbad09c81
lgb2002 <cavin12345@naver.com>
2019-03-04 20:05:09 +0900

    90

--- app/views.py
+++ app/views.py
@@ -53,7 +53,7 @@ def get_menu(day) :
 		url = "www.puhung.hs.kr/wah/main/schoolmeal/calendar.htm?menuCode=80"
 		#imsi = "http://www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+str(real_year)+"&domain.month="+str(real_month)+"&domain.day="+str(day)
 		date = "domain.year="+str(real_year)+"&domain.month="+str(real_month)+"&domain.day="+str(day)
-		html = urlopen(imsi)
+		html = urlopen(url)
 		soup = BeautifulSoup(html.read(), "html.parser")
 		test = soup.find(date)
 		test = test.get_text()
