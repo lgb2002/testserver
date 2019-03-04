fc6c7e39e504e4162f13c06fe124823787191ed4
lgb2002 <cavin12345@naver.com>
2019-03-04 20:52:50 +0900

    90

--- app/views.py
+++ app/views.py
@@ -54,7 +54,7 @@ def get_menu(day) :
 		imsi = "http://www.puhung.hs.kr/wah/main/schoolmeal/view.htm?menuCode=80&moveType=&domain.year="+str(real_year)+"&domain.month="+str(real_month)+"&domain.day="+str(day)
 		date = "domain.year="+str(real_year)+"&domain.month="+str(real_month)+"&domain.day="+str(day)
 		html = urlopen(imsi)
-		soup = BeautifulSoup(html.read(), "html.parser")
+		soup = BeautifulSoup(html, "html.parser")
 
 		print(soup)
 
