7da4d4bb1dc8628fed11949b503df48f689ca1f7
lgb2002 <cavin12345@naver.com>
2019-03-04 22:08:56 +0900

    90

--- app/views.py
+++ app/views.py
@@ -56,6 +56,7 @@ def get_menu(day) :
 		date = "domain.year="+str(real_year)+"&domain.month="+str(real_month)+"&domain.day="+str(day)
 		html = urlopen(imsi)
 		soup = BeautifulSoup(html, "html.parser")
+		print(soup)
 		test = soup.get('content')
 		#test = soup.find("div", {"class":"Schoolmeal_Cont_Cont_Cont"}).get('content')
 		#test1 = soup.get('content')
